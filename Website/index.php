af5103fbe039e027141e57ad88edd64072719537
Tushy <Tushy@users.noreply.github.com>
2017-02-05 20:43:48 +0100

    Add files via upload

---
+++ Website/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=Edge" />
+		<meta name="description" content="Cand-e-vend" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0">
+		<meta name="msapplication-tap-highlight" content="no"/>
+		<title>Cand-e-vend</title>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+	</head>
+	<body>
+		<div class="loader_container">
+			<div class="loader">
+				<div class="loader_circle"></div>
+			</div>
+		</div>
+		
+		<noscript>This site won't work without Javascript.</noscript>
+		
+		<!-- Hier wordt de content geïnjecteerd met AJAX. !-->
+		<main></main>
+
+		<script type="text/javascript" src="js/script.js"></script>
+		<script type="text/javascript" src="js/ajax.js"></script>
+		<script type="text/javascript">
+			loadContent();
+		</script>
+	</body>
+</html>
